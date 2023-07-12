3475a0ad0aaef3ab8339e3ba0c2d862d6598ec7c
reasat-rafio <rafioar96@gmail.com>
2023-07-12 13:46:05 +0600

    :beers: prettier formating not working in svelte file fix

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,18 +1,8 @@
 <script lang="ts">
-    import type { PageProps } from "@/lib/types/common.types";
+  import type { PageProps } from '@/lib/types/common.types';
 
-    export let data : PageProps<{}>
-    console.log('====================================');
-
-    console.log(data);
-
-
-
+  export let data: PageProps<{}>;
+  console.log(data);
 </script>
 
 <h1>Welcome to Artcon</h1>
-
-<div>
-
-
-</div>
