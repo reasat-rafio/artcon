a4c80f114b2debf10159fc55a30000c3e0169930
reasat-rafio <rafioar96@gmail.com>
2023-07-13 16:47:50 +0600

    :package: motion added

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -1,16 +1,12 @@
 import { siteQuery } from '@/lib/sanity/query';
 import { sanityClient } from '@/lib/sanity/sanity-client';
-import type { SiteProps } from '@/lib/types/common.types';
 import { error } from '@sveltejs/kit';
 
 export const load = async () => {
-  const data: SiteProps = await sanityClient.fetch(siteQuery);
+  const data = await sanityClient.fetch(siteQuery);
 
-  if (!data) {
-    throw error(404, {
-      message: 'Not found'
-    });
-  }
+  if (!data)
+    throw error(404, {message: 'Not found'})
 
   return {
     site: data
