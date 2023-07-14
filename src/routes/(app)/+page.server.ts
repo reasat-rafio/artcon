38fa478b84424799a4f091c83672ae01060e2b8e
reasat-rafio <rafioar96@gmail.com>
2023-07-14 18:03:17 +0600

    :alien: landing page qurey added

---
+++ src/routes/(app)/+page.server.ts
@@ -0,0 +1,30 @@
+import { asset } from '@/lib/sanity/sanity-image/query';
+import { sanityClient } from '@/lib/sanity/sanityClient';
+import { error } from '@sveltejs/kit';
+import groq from 'groq';
+
+const query = groq`
+    *[_id == "landingPage"][0]{
+        ...,
+        sections[]{
+            ...,
+            asset{
+                ...,
+                ${asset('image')},
+                video{
+                    "webm": video_webm.asset->url,
+                    "mov": video_hevc.asset->url,
+                }
+            }
+        }
+    }
+`;
+
+export const load = async () => {
+  const data = await sanityClient.fetch(query);
+  if (!data) throw error(404, { message: 'Not found' });
+
+  return {
+    page: data,
+  };
+};
