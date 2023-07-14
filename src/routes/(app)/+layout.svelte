01beb80bf9e78b007df271f3f23b941aa414357e
reasat-rafio <rafioar96@gmail.com>
2023-07-14 18:20:49 +0600

    :mag: seo component and favicon added

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,6 +1,26 @@
-<script>
-	import Navbar from '@/components/navigation/Navbar.svelte';
+<script lang="ts">
+  import Navbar from '@/components/navigation/Navbar.svelte';
+  import { urlFor } from '@/lib/sanity/sanityClient';
+  import type { SiteProps } from '@/lib/types/common.types';
+
+  export let data: { site: SiteProps };
+  let {
+    site: {
+      logos: { favicon },
+    },
+  } = data;
+
+  let faviconImage = favicon
+    ? urlFor(favicon).size(256, 256).ignoreImageParams().url()
+    : null;
+
+  console.log('====================================');
+  console.log(faviconImage);
+  console.log('====================================');
 </script>
 
+<svelte:head>
+  <link rel="icon" type="image/png" href={faviconImage} />
+</svelte:head>
 <Navbar />
 <slot />
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+  import Seo from '@/components/Seo.svelte';
   import type { PageProps } from '@/lib/types/common.types';
   import type { HomePageProps } from '@/lib/types/landing.types';
 
   export let data: PageProps<HomePageProps>;
+  let { page, site } = data;
 </script>
 
+<Seo seo={page?.seo} siteOgImg={site?.logos?.ogImage} />
 <div class="">
   <!--  -->
 </div>
