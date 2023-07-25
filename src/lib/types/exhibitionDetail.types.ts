0ef31ba08b4e747fb1575fe90db1869ab78c951f
reasat-rafio <rafioar96@gmail.com>
2023-07-25 17:39:59 +0600

    :label: types added

--- src/lib/types/exhibitionDetail.types.ts
+++ src/lib/types/exhibitionDetail.types.ts
@@ -2,11 +2,14 @@ import type DescriptionBlock from '@/components/DescriptionBlock.svelte';
 import type {
   CommonHeroProps,
   CommonImageAsset,
+  Cta,
   Quote,
   SeoProps,
   Tag,
   VRExhibitionnCta,
 } from './common.types';
+import type { SanityAsset } from '@sanity/image-url/lib/types/types';
+import type { PortableTextBlock } from 'sanity';
 
 export interface ExhinitionDetailPageProps {
   seo: SeoProps;
@@ -14,7 +17,15 @@ export interface ExhinitionDetailPageProps {
   sections: Section[];
 }
 
-type Section = CommonHeroProps | CommonImageAsset | SummaryProps;
+type Section =
+  | CommonHeroProps
+  | CommonImageAsset
+  | SummaryProps
+  | NoteProps
+  | PromotionProps
+  | ArtworkProps
+  | GalleryProps
+  | NewsAndMediaProps;
 
 export interface SummaryProps {
   _key: string;
@@ -34,4 +45,76 @@ export interface MoreInformation {
   _key: string;
   value: string;
   key: string;
+}
+
+export interface NoteProps {
+  _type: 'exhibition.note';
+  _key: string;
+  notes: Note[];
+}
+export interface Note {
+  quote: Quote;
+  _type: string;
+  _key: string;
+  descriptionBlock: NoteDescriptionBlock;
+}
+export interface NoteDescriptionBlock extends DescriptionBlock {
+  title: string;
+  subtitle: string;
+}
+
+export interface PromotionProps {
+  _key: string;
+  _type: 'exhibition.promotion';
+  quote: Quote;
+  images: SanityAsset[];
+  descriptionBlock: DescriptionBlock;
+}
+export interface PromotionDescriptionBlock extends DescriptionBlock {
+  title: string;
+  cta: Cta;
+  subtitle: string;
+  information: PortableTextBlock;
+}
+
+export interface ArtworkProps {
+  _key: string;
+  _type: 'exhibition.artwork';
+  descriptionBlock: ArtworkDescriptionBlock;
+  artworks: SanityAsset[];
+  quote: Quote;
+}
+export interface ArtworkDescriptionBlock extends DescriptionBlock {
+  title: string;
+  cta: Cta;
+  information: PortableTextBlock;
+}
+
+export interface GalleryProps {
+  _key: string;
+  _type: 'exhibition.gallery';
+  descriptionBlock: GalleryDescriptionBlock;
+  images: SanityAsset[];
+}
+export interface GalleryDescriptionBlock extends DescriptionBlock {
+  title: string;
+}
+
+export interface NewsAndMediaProps {
+  _key: string;
+  newsAndMedia: NewsAndMedia[];
+  descriptionBlock: NewsAndMediaDescriptionBlock;
+  quote: Quote;
+  _type: 'exhibition.newsAndMedia';
+}
+export interface NewsAndMediaDescriptionBlock extends DescriptionBlock {
+  title: string;
+}
+export interface NewsAndMedia {
+  image: SanityAsset;
+  subtitle: string;
+  _type: string;
+  link: string;
+  _key: string;
+  title: string;
 }
