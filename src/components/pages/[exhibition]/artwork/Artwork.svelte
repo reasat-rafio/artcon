646e7f93314239813c4773ae198aad64247814b8
reasat-rafio <rafioar96@gmail.com>
2023-07-29 14:06:09 +0600

    :construction: Artwork carousel ongoing

--- src/components/pages/[exhibition]/artwork/Artwork.svelte
+++ src/components/pages/[exhibition]/artwork/Artwork.svelte
@@ -4,6 +4,7 @@
   import Cta from '@/components/ui/Cta.svelte';
   import H4 from '@/components/ui/H4.svelte';
   import type { ArtworkProps } from '@/lib/types/exhibitionDetail.types';
+  import Artworks from './Artworks.svelte';
 
   export let props: ArtworkProps;
   let {
@@ -16,6 +17,7 @@
 <section class="py-xl">
   <div class="container">
     <Quote class="mb-xl" {quote} />
+    <Artworks class="mb-xl" {artworks} />
 
     <DescriptionBlock {description}>
       <H4 class="mb-10">{title}</H4>
---
+++ src/components/pages/[exhibition]/artwork/Artworks.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import Carousel from '@/components/ui/carousel/Carousel.svelte';
+  import SanityImage from '@/lib/sanity/sanity-image/sanity-image.svelte';
+  import { imageBuilder } from '@/lib/sanity/sanityClient';
+  import type { SanityAsset } from '@sanity/image-url/lib/types/types';
+
+  export let artworks: SanityAsset[];
+
+  console.log(artworks);
+</script>
+
+<div class="{$$props.class} ?">
+  <!-- {#each artworks as artwork}
+    <figure>
+      <SanityImage sizes="60vw" src={artwork} imageUrlBuilder={imageBuilder} />
+    </figure>
+  {/each} -->
+  <!-- <Carousel>
+    {#each artworks as artwork}
+      <figure>
+        <SanityImage
+          sizes="60vw"
+          src={artwork}
+          imageUrlBuilder={imageBuilder}
+        />
+      </figure>
+    {/each}
+  </Carousel> -->
+</div>
