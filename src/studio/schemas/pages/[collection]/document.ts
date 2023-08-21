b99a0d85f3e9222da6fbb21fe3f4912b27851c34
Al Reasat Rafio <rafioar96@gmail.com>
2023-08-21 12:22:27 +0600

    :construction: Update: collection schema

--- src/studio/schemas/pages/[collection]/document.ts
+++ src/studio/schemas/pages/[collection]/document.ts
@@ -12,6 +12,95 @@ const collection = {
       type: 'seo',
       validation: (Rule: Rule) => Rule.required(),
     },
+    {
+      name: 'name',
+      type: 'string',
+      validation: (Rule: Rule) => Rule.required(),
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      options: { source: 'name' },
+      validation: (Rule: Rule) => Rule.required(),
+    },
+    {
+      name: 'artworkImages',
+      type: 'array',
+      validation: (Rule: Rule) => Rule.required().max(4),
+      of: [
+        {
+          name: 'previewDisplayImage',
+          type: 'image',
+          options: {
+            hotspot: true,
+          },
+          validation: (Rule: Rule) => Rule.required(),
+          fields: [
+            {
+              name: 'alt',
+              title: 'Alternative Text',
+              description: 'Important for SEO and accessibility',
+              type: 'string',
+              validation: (Rule: Rule) => Rule.required(),
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'information',
+      title: 'Artwork Information',
+      type: 'array',
+      of: [{ type: 'block' }],
+      description: 'This will display in detail page and the preview page',
+      validation: (Rule: Rule) => Rule.required(),
+    },
+    {
+      name: 'provenance',
+      type: 'object',
+      validation: (Rule: Rule) => Rule.required(),
+      fields: [
+        {
+          name: 'title',
+          type: 'string',
+          validation: (Rule: Rule) => Rule.required(),
+        },
+        {
+          name: 'description',
+          type: 'array',
+          of: [{ type: 'block' }],
+          validation: (Rule: Rule) => Rule.required(),
+        },
+      ],
+    },
+    {
+      name: 'asset',
+      type: 'asset',
+      validation: (Rule: Rule) => Rule.required(),
+    },
+    {
+      name: 'previewDisplayImage',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      validation: (Rule: Rule) => Rule.required(),
+      fields: [
+        {
+          name: 'alt',
+          title: 'Alternative Text',
+          description: 'Important for SEO and accessibility',
+          type: 'string',
+          validation: (Rule: Rule) => Rule.required(),
+        },
+      ],
+    },
+    {
+      name: 'sections',
+      title: 'Sections',
+      type: 'array',
+      of: [{ type: 'common.imageAsset' }],
+    },
   ],
 };
 
