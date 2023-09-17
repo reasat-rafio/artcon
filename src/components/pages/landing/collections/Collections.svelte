5e4fad0aa3d848cc90c2f6b116c0fc14077d2405
reasat-rafio <rafioar96@gmail.com>
2023-09-17 22:06:58 +0600

    :dizzy: Home page scroll animation redo with gsap

--- src/components/pages/landing/collections/Collections.svelte
+++ src/components/pages/landing/collections/Collections.svelte
@@ -4,45 +4,71 @@
   import SanityImage from '@/lib/sanity/sanity-image/sanity-image.svelte';
   import { imageBuilder } from '@/lib/sanity/sanityClient';
   import type { CollectionsProps } from '@/lib/types/landing.types';
-  import uiStore from '@/store/ui';
+  import { onMount } from 'svelte';
   import { slide } from 'svelte/transition';
+  import { gsap } from 'gsap';
+  import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 
   export let props: CollectionsProps;
   $: ({ collections } = props);
 
   let rootEl: HTMLElement;
+  let windowWidth = 0;
   let containerEl: HTMLDivElement;
   let activeIndex: null | number = null;
-  const defaultColumWidthInPercentage = 30;
+  const defaultColumWidthInPercentage = 100 / 3;
 
-  function init(node: HTMLElement) {
-    node.style.width = `${collections.length * 50}vw`;
-  }
+  onMount(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const containerWidth = containerEl.getBoundingClientRect().width;
+    const amountToScroll = containerWidth;
+
+    const tween = gsap.to(containerEl, {
+      x: -amountToScroll,
+      duration: 3,
+      ease: 'none',
+    });
+
+    ScrollTrigger.create({
+      trigger: rootEl,
+      start: 'top top',
+      end: '+=' + amountToScroll,
+      pin: true,
+      animation: tween,
+      scrub: 3,
+      markers: true,
+    });
+  });
+
+  function init(node: HTMLElement) {}
 
   function animation(
     node: HTMLElement,
     _: { index: number; activeIndex: number | null },
   ) {
-    node.style.width = `${defaultColumWidthInPercentage}vw`;
+    // node.style.width = `${defaultColumWidthInPercentage}vw`;
   }
 </script>
 
-<section class="translate-x-[100vw]" bind:this={rootEl}>
-  <div bind:this={containerEl} use:init class="flex">
+<svelte:window bind:innerWidth={windowWidth} />
+<section class="h-screen translate-x-[100vw]" bind:this={rootEl}>
+  <div bind:this={containerEl} use:init class="flex h-full w-max">
     {#each collections as collection, index}
       {#if collection._type === 'vr'}
         <a
+          style="width: {defaultColumWidthInPercentage}vw;"
           href="/preview/vr/{collection.slug.current}"
           on:click|preventDefault={async (e) => {
-            if (e.target instanceof HTMLElement) {
-              console.log(e.target.getBoundingClientRect());
-            }
-            uiStore.setActivePreview(index);
-            await delay(600);
-            goto(`/preview/vr/${collection.slug.current}`);
+            // if (e.target instanceof HTMLElement) {
+            //   console.log(e.target.getBoundingClientRect());
+            // }
+            // uiStore.setActivePreview(index);
+            // await delay(600);
+            // goto(`/preview/vr/${collection?.slug?.current}`);
           }}
           use:animation={{ index, activeIndex }}
-          class="relative h-screen"
+          class="relative h-full"
         >
           <SanityImage
             class="h-full w-full object-cover"
@@ -53,12 +79,12 @@
           />
         </a>
 
-        {#if index === $uiStore.seclectedPreviewIndex}
+        <!-- {#if index === $uiStore.seclectedPreviewIndex}
           <div
             in:slide={{ axis: 'x', duration: 400 }}
             class="w-[70vw] bg-white"
           />
-        {/if}
+        {/if} -->
       {/if}
     {/each}
   </div>
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -4,47 +4,16 @@
   import type { PageProps } from '@/lib/types/common.types';
   import type { HomePageProps } from '@/lib/types/landing.types';
   import Collections from '@/components/pages/landing/collections/Collections.svelte';
-  import { tweened } from 'svelte/motion';
-  import { expoOut } from 'svelte/easing';
-  import uiStore from '@/store/ui';
 
-  type MouseEvnt = WheelEvent & {
-    currentTarget: EventTarget & Window;
-  };
   export let data: PageProps<HomePageProps>;
   let {
     page,
     site: { logos },
   } = data;
-
-  let rootEl: HTMLDivElement;
-  let windowWidth = 0;
-  const SCROLL_AMOUNT = 600;
-  const tweenedScrollAmount = tweened(0, { duration: 2500, easing: expoOut });
-  $: if (rootEl) rootEl.scrollLeft = $tweenedScrollAmount;
-
-  const scrollAction = (event: MouseEvnt) => {
-    const scrollingUp = event.deltaY > 0 ? true : false;
-    if (scrollingUp) tweenedScrollAmount.set(rootEl.scrollLeft + SCROLL_AMOUNT);
-    else tweenedScrollAmount.set(rootEl.scrollLeft - SCROLL_AMOUNT);
-  };
-
-  const sceollActiveSlidesPosition = () => {
-    if ($uiStore.seclectedPreviewIndex == null) return;
-
-    const offSetWidth = (windowWidth / 100) * 30;
-    tweenedScrollAmount.set(
-      windowWidth + offSetWidth * $uiStore.seclectedPreviewIndex,
-      { duration: 600 },
-    );
-  };
-
-  $: $uiStore.seclectedPreviewIndex, sceollActiveSlidesPosition();
 </script>
 
-<svelte:window bind:innerWidth={windowWidth} on:wheel={scrollAction} />
 <Seo seo={page?.seo} siteOgImg={logos?.ogImage} />
-<div class="fixed inset-0 h-screen w-screen overflow-hidden" bind:this={rootEl}>
+<div class="overflow-hidden">
   {#each page.sections as s}
     {#if s._type === 'common.hero'}
       <Hero class="fixed inset-0" props={s} />
