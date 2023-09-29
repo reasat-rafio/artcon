75bb3c70a307f1f08a789e4e34b7ba1f56312d5f
Al Reasat Rafio <rafioar96@gmail.com>
2023-09-29 19:52:32 +0600

    :safety_vest: Validation added for artist summary

--- src/studio/schemas/pages/[artist]/artist-details/site-documents/siteDocuments.ts
+++ src/studio/schemas/pages/[artist]/artist-details/site-documents/siteDocuments.ts
@@ -15,6 +15,18 @@ const siteDocuments = {
       name: 'sections',
       title: 'Sections',
       type: 'array',
+      validation: (Rule: Rule) =>
+        Rule.custom(
+          (sections: { _type: string; [_key: string]: unknown }[]) => {
+            const summaryCount = sections.filter(
+              (section) => section._type === 'artist.summary',
+            ).length;
+            if (summaryCount < 1) {
+              return 'Artist summary is required".';
+            }
+            return true;
+          },
+        ),
       of: [
         { type: 'common.hero' },
         { type: 'common.imageAsset' },
