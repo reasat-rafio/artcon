55c60073775563f1d16fed7ac94ec294dee90990
reasat-rafio <rafioar96@gmail.com>
2023-09-30 02:32:19 +0600

    :recycle: W-nav layout update

--- src/routes/(app)/(w-navbar)/+layout.svelte
+++ src/routes/(app)/(w-navbar)/+layout.svelte
@@ -1,18 +1,14 @@
 <script lang="ts">
-  import Footer from '@/components/footer/Footer.svelte';
   import type { SiteProps } from '@/lib/types/common.types';
   import Navbar from '@/components/navigation/Navbar.svelte';
 
   export let data: { site: SiteProps };
   $: ({
     site: {
-      logos: { logoLight, logoDark },
-      footer,
-      contact,
+      logos: { logoDark },
     },
   } = data);
 </script>
 
 <Navbar logo={logoDark} />
 <slot />
-<!-- <Footer {footer} {contact} logo={logoLight} /> -->
