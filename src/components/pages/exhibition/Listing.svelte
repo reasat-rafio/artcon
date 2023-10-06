06628a9d315707c2693294011179286b647d3c57
Al Reasat Rafio <rafioar96@gmail.com>
2023-10-06 22:16:56 +0600

    :iphone: Responsivenessimprove

--- src/components/pages/exhibition/Listing.svelte
+++ src/components/pages/exhibition/Listing.svelte
@@ -9,38 +9,47 @@
 </script>
 
 <div>
-  {#each list as { items, image }}
-    <section class="py-section container">
-      <div class="grid grid-cols-3 gap-x-[25px] gap-y-[56px]">
-        {#each items as { name, slug, tag, type, previewDisplayImage, _id } (_id)}
-          <div in:fade out:fade={{ duration: 0 }}>
-            <Card
-              el="a"
-              href={`/exhibition/${slug.current}`}
-              class="space-y-[20px] pl-[20px]"
-              let:Image
-              let:Title
-              let:Container
-              let:Subtitle
-            >
-              <Image image={previewDisplayImage} />
-              <Container>
-                <Title
-                  >{name} / {#if typeof type === 'string'}
-                    {type}
-                  {:else}
-                    {type.name}
-                  {/if}</Title
-                >
-                <Subtitle class="text-sonic-silver">{tag.name}</Subtitle>
-              </Container>
-            </Card>
-          </div>
-        {/each}
-      </div>
-    </section>
-    {#if !!image}
-      <ImageAsset props={image} />
-    {/if}
-  {/each}
+  {#if !!list?.length}
+    {#each list as { items, image }}
+      <section class="py-section container">
+        <div
+          class="grid grid-cols-1 gap-x-[25px] gap-y-[56px] md:grid-cols-2 lg:grid-cols-3"
+        >
+          {#each items as { name, slug, tag, type, previewDisplayImage, _id } (_id)}
+            <div in:fade out:fade={{ duration: 0 }}>
+              <Card
+                el="a"
+                href={`/exhibition/${slug.current}`}
+                class="space-y-[20px] pl-[20px]"
+                let:Image
+                let:Title
+                let:Container
+                let:Subtitle
+              >
+                <Image image={previewDisplayImage} />
+                <Container>
+                  <Title
+                    >{name} / {#if typeof type === 'string'}
+                      {type}
+                    {:else}
+                      {type.name}
+                    {/if}</Title
+                  >
+                  <Subtitle class="text-sonic-silver">{tag.name}</Subtitle>
+                </Container>
+              </Card>
+            </div>
+          {/each}
+        </div>
+      </section>
+
+      {#if !!image}
+        <ImageAsset props={image} />
+      {/if}
+    {/each}
+  {:else}
+    <div class="py-section container">
+      <h3 class="head-3xl">No Exhibition Found</h3>
+    </div>
+  {/if}
 </div>
