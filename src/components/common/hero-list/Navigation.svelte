3ced401030cd691fd399c135829e6be5b3f7776c
Al Reasat Rafio <rafioar96@gmail.com>
2023-11-26 13:01:08 +0600

    :fire: Listing hero slider navigation button remove

--- src/components/common/hero-list/Navigation.svelte
+++ src/components/common/hero-list/Navigation.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import ChevronLeftRounded from '@/components/icons/ChevronLeftRounded.svelte';
   import ChevronRightRounded from '@/components/icons/ChevronRightRounded.svelte';
   import { zeroPad } from '@/lib/helper';
   import { createEventDispatcher } from 'svelte';
@@ -14,30 +13,23 @@
 </script>
 
 <nav class="absolute bottom-0 left-0 z-20 w-full">
-  <div class="flex justify-between px-5 py-5 text-white lg:py-10">
-    <button on:click={() => dispatch('sliedPrev')}>
-      <ChevronLeftRounded color="white" />
-    </button>
+  <div class="flex justify-center px-5 py-5 text-white lg:py-10">
     <div
-      class="flex items-center space-x-1 font-optiberling-agency font-medium"
-    >
+      class="flex items-center space-x-1 font-optiberling-agency font-medium">
       {#key activeBlockIndex}
-        <span
+        <button
+          on:click={() => dispatch('sliedPrev')}
           in:fly={{
             y: scrollDirection === 'forward' ? 5 : -5,
             duration: 600,
             easing: sineOut,
-          }}
-        >
+          }}>
           {zeroPad(activeBlockIndex + 1)}
-        </span>
+        </button>
       {/key}
-      <span>
+      <button on:click={() => dispatch('slideNext')}>
         / {zeroPad(blocksLength)}
-      </span>
+      </button>
     </div>
-    <button on:click={() => dispatch('slideNext')}>
-      <ChevronRightRounded color="white" />
-    </button>
   </div>
 </nav>
