aee377fd8dfa085fdcc49fd08d742d6a74ce554f
Al Reasat Rafio <rafioar96@gmail.com>
2023-12-05 14:46:22 +0600

    :zap: No search result found state created for artist

--- src/components/pages/artist/list/List.svelte
+++ src/components/pages/artist/list/List.svelte
@@ -3,6 +3,7 @@
     Artist as ArtistProps,
     SortedArtists,
   } from '@/lib/types/artist.types';
+  import { fade } from 'svelte/transition';
   import Artist from './Artists.svelte';
   import NavigationAnchorDesktop from './nav-anchor/NavigationAnchorDesktop.svelte';
   import NavigationAnchorMobile from './nav-anchor/NavigationAnchorMobile.svelte';
@@ -38,19 +39,34 @@
 <section>
   <div
     class="container-primary flex pb-[2.5rem] pt-[3.12rem] max-xl:flex-col sm:pb-[4.5rem] lg:gap-x-[2rem] xl:gap-x-[6rem] xl:pb-[9rem] 2xl:gap-x-[11.44rem]">
-    <NavigationAnchorMobile
-      class="block xl:hidden"
-      {anchors}
-      {activeAnchor}
-      {activeAnchorIndex} />
-    <Artist
-      class="flex-1 max-2xl:space-y-[1.25rem]"
-      bind:activeAnchor
-      artistGroup={sortedArtistWithTitleAsFirstLetter} />
-    <NavigationAnchorDesktop
-      class="hidden xl:flex"
-      {anchors}
-      {activeAnchor}
-      {activeAnchorIndex} />
+    {#if !!artists?.length}
+      <NavigationAnchorMobile
+        class="block xl:hidden"
+        {anchors}
+        {activeAnchor}
+        {activeAnchorIndex} />
+    {/if}
+    {#if !!artists?.length}
+      <Artist
+        class="flex-1 max-2xl:space-y-[1.25rem]"
+        bind:activeAnchor
+        artistGroup={sortedArtistWithTitleAsFirstLetter} />
+    {:else}
+      <div in:fade>
+        <h2
+          class="pt-[10vh] text-[1rem] font-medium leading-[115.5%] tracking-[0.04rem] text-[#77777C] md:text-[1.5rem] xl:text-[2rem]">
+          Looks like we couldn't find what you're looking for. Try refining your
+          search criteria.
+        </h2>
+      </div>
+    {/if}
+
+    {#if !!artists?.length}
+      <NavigationAnchorDesktop
+        class="hidden xl:flex"
+        {anchors}
+        {activeAnchor}
+        {activeAnchorIndex} />
+    {/if}
   </div>
 </section>
