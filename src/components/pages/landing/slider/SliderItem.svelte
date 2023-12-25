064e54af76c1d4cfc4641cd9c785611bdb90a7c1
reasat-rafio <rafioar96@gmail.com>
2023-12-25 23:14:58 +0600

    :card_file_box: Documentary page rename to audio visual in the cms

--- src/components/pages/landing/slider/SliderItem.svelte
+++ src/components/pages/landing/slider/SliderItem.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import type { SliderProps, WithExtra } from '@/lib/types/landing.types';
-  import SliderItemLayout from './SliderItemLayout.svelte';
+  import SliderItemWrapper from './SliderItemWrapper.svelte';
   import { calculateStatusBetweenDates } from '@/lib/helper';
 
   export let props: WithExtra<SliderProps>;
@@ -26,7 +26,7 @@
         }`;
 </script>
 
-<SliderItemLayout {index} {sliderImageVideo} {href}>
+<SliderItemWrapper {index} {sliderImageVideo} {href}>
   <div
     class="absolute bottom-0 left-0 z-10 space-y-[0.62rem] text-white transition-transform duration-500 ease-out group-hover:translate-y-[-0.8rem] max-lg:px-[1.25rem] max-lg:pb-[2.8rem] lg:pb-[5rem] lg:pl-[3.38rem] xl:w-[90%]">
     <h2 class="head-6">{title}</h2>
@@ -60,4 +60,4 @@
       {/if}
     </div>
   </div>
-</SliderItemLayout>
+</SliderItemWrapper>
