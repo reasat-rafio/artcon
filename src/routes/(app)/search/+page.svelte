cdf2725193faad4f4cc0ab6c858108ac0148b95d
reasat-rafio <rafioar96@gmail.com>
2023-12-25 15:43:10 +0600

    :hammer: Tweak

--- src/routes/(app)/search/+page.svelte
+++ src/routes/(app)/search/+page.svelte
@@ -2,7 +2,6 @@
   import NoSearchResultFound from '@/components/common/NoSearchResultFound.svelte';
   import Seo from '@/components/common/Seo.svelte';
   import Footer from '@/components/common/footer/Footer.svelte';
-  import Navbar from '@/components/common/navigation/Navbar.svelte';
   import Collection from '@/components/pages/search/Collection.svelte';
   import Documentary from '@/components/pages/search/Documentary.svelte';
   import Event from '@/components/pages/search/Event.svelte';
