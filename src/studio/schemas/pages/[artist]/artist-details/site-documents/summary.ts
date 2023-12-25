419be0ef89de6e70a17c3049b33ab8910620f3a1
reasat-rafio <rafioar96@gmail.com>
2023-12-25 21:05:59 +0600

    :card_file_box: Scheam adjustment

--- src/studio/schemas/pages/[artist]/artist-details/site-documents/summary.ts
+++ src/studio/schemas/pages/[artist]/artist-details/site-documents/summary.ts
@@ -37,7 +37,7 @@ const summary = {
             },
             {
               name: 'caption',
-              type: 'text',
+              type: 'string',
               validation: (Rule: Rule) => Rule.required(),
             },
           ],
