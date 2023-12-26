55dd0e58ab51216884c875bee68b384488df0936
reasat-rafio <rafioar96@gmail.com>
2023-12-26 17:08:31 +0600

    :building_construction: Replaced font assets

--- src/components/common/hero/Hero.svelte
+++ src/components/common/hero/Hero.svelte
@@ -4,7 +4,6 @@
   import { gsap } from 'gsap';
   import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
   import { onMount } from 'svelte';
-  import ChevronDown from '../../icons/ChevronDown.svelte';
   import Cta from '../../ui/Cta.svelte';
   import Asset from './Asset.svelte';
   import Overlay from './Overlay.svelte';
