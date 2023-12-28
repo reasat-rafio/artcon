e19c89ad7384dd875f73401678f28122a24ec322
reasat-rafio <rafioar96@gmail.com>
2023-12-28 14:10:30 +0600

    :adhesive_bandage: Zindex issue fix

--- src/components/common/navigation/nav-items/NavItems.svelte
+++ src/components/common/navigation/nav-items/NavItems.svelte
@@ -24,7 +24,7 @@
 
 <svelte:window bind:innerHeight />
 <aside
-  class="!fixed left-0 top-1/2 z-[1004] hidden h-full w-[11rem] -translate-y-1/2 items-center lg:flex 2xl:w-[16rem]">
+  class="!fixed left-0 top-1/2 z-[1004] hidden h-fit w-[11rem] -translate-y-1/2 items-center lg:flex 2xl:w-[16rem]">
   <nav class="ml-[2.5rem] flex flex-col gap-y-[1.25rem]">
     {#each menu as { title, externalUrl, pageUrl }}
       <Item {title} {pageUrl} {externalUrl} />
