01c1bba3953ced5bb1557c0d8fc6d0b834f52b2c
reasat-rafio <rafioar96@gmail.com>
2024-01-16 00:12:20 +0600

    :construction: /artowrks page created

---
+++ src/routes/(app)/(w-navbar)/(smooth-scroll)/exhibition/[slug]/artworks/+page.server.ts
@@ -0,0 +1,44 @@
+import { asset } from '@/lib/sanity/sanity-image';
+import { sanityClient } from '@/lib/server/sanity/serverSanityClient';
+import { error, type ServerLoad } from '@sveltejs/kit';
+import groq from 'groq';
+
+const query = (params: Partial<Record<string, string>>) =>
+  groq`*[_type== "exhibition" && slug.current == "${params.slug}"] [0]{
+    name,
+    topTitle,
+    subtitle,
+    cta,
+    seo,
+    endDate,
+    startDate,
+    tag->,
+    asset {
+      ...,
+      ${asset('image')},
+      video{
+          "webm": video_webm.asset->url,
+          "mov": video_hevc.asset->url,
+      }
+    },
+    artworks[]->{
+      information,
+      "artwork": artworkImages[0] {
+        ...,
+        asset->{
+          ...,
+          metadata {
+           lqip,
+          dimensions
+         }
+       }
+      }
+    },
+  }`;
+
+export const load: ServerLoad = async ({ params }) => {
+  const data = await sanityClient.fetch(query(params));
+  if (!data) throw error(404, 'Not found');
+
+  return { page: data };
+};
