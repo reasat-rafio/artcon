8f274318e6434f5ad97afe2570f604047364d0ed
reasat-rafio <rafioar96@gmail.com>
2024-01-22 18:36:12 +0600

    :building_construction: Navbar grouping remove and navbar added to the root layout

--- src/routes/(app)/(smooth-scroll)/(navbar)/+layout.server.ts
+++
@@ -1,5 +0,0 @@
-export const load = async ({ url: { pathname } }) => {
-  return {
-    pathname: pathname,
-  };
-};
--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -3,9 +3,9 @@ import { sanityClient } from '@/lib/sanity/sanityClient';
 import type { SiteProps } from '@/lib/types/common.types';
 import { error } from '@sveltejs/kit';
 
-export const load = async () => {
+export const load = async ({ url: { pathname } }) => {
   const data = (await sanityClient.fetch(siteQuery)) as SiteProps;
   if (!data) throw error(404, { message: 'Not found' });
 
-  return { site: data };
+  return { site: data, pathname };
 };
