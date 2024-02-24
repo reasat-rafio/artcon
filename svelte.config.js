cf879cb73ef841c4d1d66e8d32288c67c317c1a3
reasat-rafio <rafioar96@gmail.com>
2024-02-24 19:25:26 +0600

    :rewind: Reverted to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-// import adapter from '@sveltejs/adapter-vercel';
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
