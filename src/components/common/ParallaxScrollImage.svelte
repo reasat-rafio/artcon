ce978cc8607a7ae69a2ec221028cd257aa44c25e
mohammadmunnamia0 <rafioar96@gmail.com>
2025-12-08 13:58:11 +0600

    Refactor ParallaxScrollImage component for improved image responsiveness and layout

--- src/components/common/ParallaxScrollImage.svelte
+++ src/components/common/ParallaxScrollImage.svelte
@@ -68,9 +68,9 @@
       <figure
         bind:this={firstImageEl}
         on:click={() => firstImage?.triggerPopup && dispatch('triggerPopup')}
-        class="overflow-hidden rounded-xl">
+        class="overflow-hidden rounded-xl max-w-[30vw]">
         <SanityImage
-          class="aspect-square w-full rounded-xl object-cover transition-transform duration-300 hover:scale-110 {firstImage?.triggerPopup
+          class="max-h-[500px] w-full h-auto rounded-xl object-contain transition-transform duration-300 hover:scale-110 {firstImage?.triggerPopup
             ? 'cursor-pointer'
             : 'cursor-default'}"
           sizes="30vw"
@@ -91,7 +91,7 @@
         class="ml-auto w-[13.4375rem] overflow-hidden"
         on:click={() => firstImage?.triggerPopup && dispatch('triggerPopup')}>
         <SanityImage
-          class="aspect-square rounded-xl object-cover transition-transform duration-300 hover:scale-110 {firstImage?.triggerPopup
+          class="w-full h-auto rounded-xl object-contain transition-transform duration-300 hover:scale-110 {firstImage?.triggerPopup
             ? 'cursor-pointer'
             : 'cursor-default'}"
           sizes="70vw"
@@ -106,17 +106,17 @@
       </figure>
     </div>
   </div>
-  <div class="flex-1 xl:pr-[9.69rem]">
+  <div class="w-full flex-1 xl:pr-[9.69rem]">
     <svelte:element
       this={!!secondImage?.link ? 'a' : 'div'}
       href={secondImage?.link}
       target="_blank">
-      <figure class="h-full 3xl:h-[55.83069rem] overflow-hidden rounded-xl">
+      <figure class="overflow-hidden rounded-xl">
         <SanityImage
           sizes="(min-width:1024px) 70vw, 100vw"
           src={secondImage.img}
           alt={secondImage.img?.alt}
-          class="h-full w-full rounded-xl object-cover transition-transform duration-300 hover:scale-110"
+          class="w-full h-auto rounded-xl object-contain transition-transform duration-300 hover:scale-110"
           imageUrlBuilder={imageBuilder} />
         {#if !!secondImage.img?.caption || !!secondImage?.caption}
           <figurecaption class="caption">
