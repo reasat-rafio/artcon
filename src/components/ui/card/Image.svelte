d49ffc078ce612ca3996777dbc9872d8c86ee8db
mohammadmunnamia0 <rafioar96@gmail.com>
2026-01-12 15:37:01 +0600

    Increase transition duration for image hover effects to enhance visual smoothness

--- src/components/ui/card/Image.svelte
+++ src/components/ui/card/Image.svelte
@@ -13,7 +13,7 @@
     class={cn('h-auto max-3xl:aspect-video 3xl:h-[14.125rem] overflow-hidden rounded-[12px]', $$props.class)}>
     <SanityImage
       imageUrlBuilder={imageBuilder}
-      class="h-full w-full rounded-[12px] object-cover transition-transform duration-300 hover:scale-105"
+      class="h-full w-full rounded-[12px] object-cover transition-transform duration-600 hover:scale-105"
       sizes="(min-width:640px) 35vw, 100vw"
       src={image}
       alt={image?.alt} />
@@ -22,7 +22,7 @@
   <figure class={cn('h-auto sm:h-[25rem] overflow-hidden rounded-[12px]', $$props.class)}>
     <SanityImage
       imageUrlBuilder={imageBuilder}
-      class="h-full w-full object-contain object-bottom transition-transform duration-300 hover:scale-105"
+      class="h-full w-full object-contain object-bottom transition-transform duration-600 hover:scale-105"
       sizes="(min-width:640px) 35vw, 100vw"
       src={image}
       alt={image?.alt} />
